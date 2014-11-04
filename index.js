3d96fb851b45ee1f25be32447c899b154094f8cf
Marcel Erz <erz@yahoo-inc.com>
2014-11-03 22:49:47 -0800

    Add service objects

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 // Copyright 2014, Yahoo! Inc.
 // Copyrights licensed under the Mit License. See the accompanying LICENSE file for terms.
 
-var DriverAdapter = require('./lib/driverAdapter/driverAdapter');
-var CabbieAdapter = require('./lib/driverAdapter/cabbieAdapter');
-
-var BaseObject = require('./lib/baseObject');
-var GenericPageObject = require('./lib/genericPageObject');
-var PageObject = require('./lib/pageObject');
-var PanelObject = require('./lib/panelObject');
-var ViewObject = require('./lib/viewObject');
-
 module.exports = {
-	DriverAdapter: DriverAdapter,
+
+	// Driver-adapters
+	DriverAdapter: require('./lib/driverAdapter/driverAdapter'),
 	driverAdapters: {
-		"Cabbie": CabbieAdapter
+		"Cabbie": require('./lib/driverAdapter/cabbieAdapter')
 	},
 
-	BaseObject: BaseObject,
-	GenericPageObject: GenericPageObject,
-	PageObject: PageObject,
-	ViewObject: ViewObject,
-	PanelObject: PanelObject,
+	// Page-Objects
+	BaseObject: require('./lib/pageObjects/baseObject'),
+	GenericPageObject: require('./lib/pageObjects/genericPageObject'),
+	PageObject: require('./lib/pageObjects/pageObject'),
+	ViewObject: require('./lib/pageObjects/viewObject'),
+	PanelObject: require('./lib/pageObjects/panelObject'),
+
+	// Service-Objects
+	ServiceEndPoint: require('./lib/service/endPoint'),
+	ServiceEntry: require('./lib/service/entry'),
+	ServiceNewEntry: require('./lib/service/newEntry'),
+	ServiceList: require('./lib/service/list'),
+	ServiceLookup: require('./lib/service/lookup'),
+
+	// Store-Objects
+	StoreBase: require('./lib/store/base'),
+	StoreLookup: require('./lib/store/lookup'),
 
 	version: require('./package.json').version
 };
