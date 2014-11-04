47c405bbd09380ebfa0e6fef7b7f1de7df87c903
Marcel Erz <erz@yahoo-inc.com>
2014-11-04 12:43:32 -0800

    Add API-documentation and Coverage report

---
+++ docs/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "BaseObject",
+        "CabbieAdapter",
+        "DriverAdapter",
+        "GenericPageObject",
+        "PageObject",
+        "PanelObject",
+        "ServiceBaseObject",
+        "ServiceContainer",
+        "ServiceContainerObject",
+        "ServiceEndPoint",
+        "ServiceEndPointObject",
+        "ServiceEntry",
+        "ServiceList",
+        "ServiceLookup",
+        "ServiceNewEntry",
+        "ServiceObject",
+        "ServiceRequest",
+        "StoreBase",
+        "StoreLookup",
+        "ViewObject"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
